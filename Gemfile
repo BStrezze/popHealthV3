ef915ea10831bfb9f2aec1632916485d7eaa2acd
Brian Strezze <bstrezze@gosmcpartners.com>
2014-06-24 09:50:15 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,71 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.2.18'
+
+# Changed for testing
+# gem 'quality-measure-engine', '3.0.2'
+gem 'quality-measure-engine', path: '../quality-measure-engine-v3.0.2'
+
+gem 'health-data-standards', '3.4.6'
+gem 'nokogiri'
+gem 'rubyzip'
+
+# Should be removed in the future. This is only used for the
+# admin log page. When the admin pages are switched to
+# client side pagination, this can go away.
+gem 'will_paginate'
+
+gem "active_model_serializers"
+
+gem 'json', :platforms => :jruby
+
+gem "mongoid"
+
+gem 'highline'
+
+gem 'devise'
+
+gem 'git'
+
+gem 'foreman'
+gem "thin"
+gem 'formtastic'
+gem 'cancan'
+gem 'factory_girl', "2.6.3"
+gem 'apipie-rails'
+
+# backport fixes from future versions of Sprockets into a Rails 3-compatible gem
+gem 'sprockets', '2.2.2.backport1'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'less-rails'
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+  gem "bootstrap-sass"
+end
+
+group :test, :develop, :ci do
+  gem 'pry'
+  gem 'jasmine', '2.0.0'
+  gem 'turn', :require => false
+  gem 'simplecov', :require => false
+  gem 'mocha', :require => false
+  gem "unicorn", :platforms => [:ruby, :jruby]
+  gem 'minitest', "~> 4.0"
+end
+
+group :test, :develop do
+  gem 'pry-debugger'
+end
+
+group :production do
+  gem 'libv8', '~> 3.16.14.3'
+  gem 'therubyracer', '~> 0.12.0', :platforms => [:ruby, :jruby] # 10.8 mountain lion compatibility
+end
+
+gem 'handlebars_assets'
+# FIXME remove this when we don't need old versions of jquery/backbone/underscore anymore
+gem 'jquery-rails'
---
+++ app/controllers/api/reports_controller.rb
@@ -0,0 +1,56 @@
+module Api
+
+  class ReportsController < ApplicationController
+    resource_description do
+      short 'Reports'
+      formats ['xml']
+      description <<-RCDESC
+        This resource is responsible for the generation of QRDA Category III reports from clincial
+        quality measure calculations.
+      RCDESC
+    end
+    before_filter :authenticate_user!
+    skip_authorization_check
+
+    api :GET, '/reports/qrda_cat3.xml', "Retrieve a QRDA Category III document"
+    param :measure_ids, Array, :desc => 'The HQMF ID of the measures to include in the document', :required => false
+    param :effective_date, String, :desc => 'Time in seconds since the epoch for the end date of the reporting period',
+                                   :required => false
+    param :provider_id, String, :desc => 'The Provider ID for CATIII generation'
+    description <<-CDESC
+      This action will generate a QRDA Category III document. If measure_ids and effective_date are not provided,
+      the values from the user's dashboard will be used.
+    CDESC
+    def cat3
+      measure_ids = params[:measure_ids] ||current_user.preferences["selected_measure_ids"]
+      filter = measure_ids=="all" ? {}  : {:hqmf_id.in =>measure_ids}
+      exporter =  HealthDataStandards::Export::Cat3.new
+      effective_date = params["effective_date"] || current_user.effective_date || Time.gm(2012, 12, 31)
+      end_date = Time.at(effective_date.to_i)
+      provider = provider_filter = nil
+      if params[:provider_id].present?
+        provider = Provider.find(params[:provider_id])
+        provider_filter = {}
+        provider_filter['filters.providers'] = params[:provider_id] if params[:provider_id].present?
+      end
+      render xml: exporter.export(HealthDataStandards::CQM::Measure.top_level.where(filter),
+                                   generate_header(provider),
+                                   effective_date.to_i,
+                                   end_date.years_ago(1),
+                                   end_date, provider_filter), content_type: "attachment/xml"
+    end
+
+    private
+
+    def generate_header(provider)
+      header = Qrda::Header.new(APP_CONFIG["cda_header"])
+
+      header.identifier.root = UUID.generate
+      header.authors.each {|a| a.time = Time.now}
+      header.legal_authenticator.time = Time.now
+      header.performers << provider
+
+      header
+    end
+  end
+end
